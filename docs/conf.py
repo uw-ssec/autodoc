e0e3a356859d8003b26d89439c1989e664c775dd
Carlos Garcia Jurado Suarez <carlosg@uw.edu>
2023-11-21 14:59:06 -0800

    Fix lints

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,12 @@
 
 import os
 import sys
-
-import autoapi
 from importlib.metadata import version
 
+import autoapi
+
 # Define path to the code to be documented **relative to where conf.py (this file) is kept**
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -32,11 +32,11 @@ extensions.append("autoapi.extension")
 extensions.append("nbsphinx")
 
 templates_path = []
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 
 master_doc = "index"  # This assumes that sphinx-build is called from the root directory
 html_show_sourcelink = False  # Remove 'view source code' from top of page (for html, not python)
-add_module_names = False # Remove namespaces from class/method signatures
+add_module_names = False  # Remove namespaces from class/method signatures
 
 autoapi_type = "python"
 autoapi_dirs = ["../src"]
