667e77e0ba9370cb3f17be3838cb674378ab01cc
Anuj Sinha <sinha.anuj30@gmail.com>
2024-01-31 15:27:53 -0800

    refactor: use @dataclass annotation

--- src/autora/doc/classes/EvalResult.py
+++ src/autora/doc/classes/EvalResult.py
@@ -1,21 +1,12 @@
+from dataclasses import dataclass
 from typing import List, Optional
 
 
+@dataclass
 class EvalResult:
-    def __init__(
-        self,
-        prediction: List[str],
-        prompt: str,
-        bleu_score: Optional[float] = None,
-        meteor_score: Optional[float] = None,
-    ):
-        self.prediction = prediction
-        self.prompt = prompt
-        self.bleu_score = bleu_score
-        self.meteor_score = meteor_score
+    """Class for storing LLM evaluation results"""
 
-    def __str__(self) -> str:
-        return (
-            f"prediction: {self.prediction}, prompt: {self.prompt},"
-            f"bleu_score: {self.bleu_score}, meteor_score: {self.meteor_score} )"
-        )
+    prediction: List[str]
+    prompt: str
+    bleu_score: Optional[float]
+    meteor_score: Optional[float]
